9b07a78e8f50d9f5c1c7bca58ef5988e2e93257d
sanat kumar <sanatkumar166@gmail.com>
2021-06-29 02:24:37 +0530

    first and final

--- src/App.js
+++ src/App.js
@@ -1,24 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import {React,useState} from "react"
+
 
 function App() {
+  const [ toDo, setTodo ] = useState();
+  const [toDoes, setToDoes]= useState([]);
+
+  const AddTodo=()=>{
+    let temp = toDoes;
+    temp.push(toDo);
+    setToDoes(temp);
+    setTodo("");
+
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <>
+    <div style={{display:"flex",justifyContent:"center",justifyItems:"center"}}>
+      <div style={{display:""}}>
+    <div style={{display:"flex",justifyContent:"center"}}>
+      Todo
     </div>
+    <div style={{display:"flex"}}>
+      <input type="text" value={toDo} onChange={(e)=>setTodo(e.target.value)} placeholder="task"></input>
+      <button className="btn sm" onClick={AddTodo}> Add</button>
+    </div>
+    <div>
+      {toDoes.map((data,i)=> <div> {i+1 + ".  " +data} </div>)}
+    </div>
+    </div>
+    </div>
+    </>
   );
 }
 
